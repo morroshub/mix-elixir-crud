5d1e62c54bca489b0ab8c3cb06a11e2d5b590982
morroshub <lgmorrone@icloud.com>
2023-12-10 19:15:28 -0300

    creacion de modulos writer and reader

---
+++ pprojecto/lib/Empresa.ex
@@ -0,0 +1,10 @@
+defmodule Empresa do
+  @moduledoc """
+  Documentation for `crudmix` is a crud for a small business.
+  It is available to have 4 fields to be filled.
+  """
+
+  defe Empleado do
+    defstruct [:nombre, :apellido, :id, :sector, :turno, :sueldo]
+  end
+end
